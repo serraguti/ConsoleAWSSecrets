9206bb10d8047f87edda293d17c3430a8d53ea88
serraguti <pacoserranox@gmail.com>
2024-05-22 14:23:01 +0200

    Add project files.

---
+++ ConsoleAWSSecrets/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAWSSecrets.Helpers;
+using ConsoleAWSSecrets.Models;
+using Newtonsoft.Json;
+
+Console.WriteLine("Ejemplo secrets manager");
+string miSecreto = await HelperSecretManager.GetSecretAsync();
+Console.WriteLine(miSecreto);
+
+//PODEMOS DAR FORMATO A NUESTRO SECRET PARA PODER UTILIZARLO COMO CLASE
+KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
+Console.WriteLine("MySql: " + model.Mysql);
+
