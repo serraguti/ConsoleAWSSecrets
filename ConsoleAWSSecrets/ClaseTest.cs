f5a23efda5a2554293ab258115c0b23e63514331
serraguti <pacoserranox@gmail.com>
2024-05-23 09:58:03 +0200

    commit

---
+++ ConsoleAWSSecrets/ClaseTest.cs
@@ -0,0 +1,24 @@
+﻿using ConsoleAWSSecrets.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAWSSecrets
+{
+    public class ClaseTest
+    {
+        private KeysModel keys;
+
+        public ClaseTest(KeysModel keys)
+        {
+            this.keys = keys;
+        }
+
+        public string GetValue()
+        {
+            return this.keys.Api;
+        }
+    }
+}
--- ConsoleAWSSecrets/Program.cs
+++ ConsoleAWSSecrets/Program.cs
@@ -1,5 +1,7 @@
-﻿using ConsoleAWSSecrets.Helpers;
+﻿using ConsoleAWSSecrets;
+using ConsoleAWSSecrets.Helpers;
 using ConsoleAWSSecrets.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 Console.WriteLine("Ejemplo secrets manager");
@@ -10,3 +12,21 @@ Console.WriteLine(miSecreto);
 KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
 Console.WriteLine("MySql: " + model.Mysql);
 
+
+//ALMACENAMOS EL OBJETO KEYSMODEL DENTRO DE DY
+//ESTO LO HARIAMOS EN CUALQUIER APP API O MVC EN Program
+var provider =
+    new ServiceCollection()
+    .AddTransient<ClaseTest>()
+    .AddSingleton<KeysModel>(x => model)
+    .BuildServiceProvider();
+
+//EN CUALQUIER CLASE PODEMOS RECUPERAR LAS KEYS, POR EJEMPLO, 
+//EN EL PROPIO PROGRAM SI NECESITAMOS CONNECTIONSTRING O EN 
+//CUALQUIER OTRA CLASE DE INYECCION
+var service = provider.GetService<KeysModel>();
+string connectionString = service.Mysql;
+Console.WriteLine(connectionString);
+
+var test = provider.GetService<ClaseTest>();
+Console.WriteLine("Api: " + test.GetValue());
